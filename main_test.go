4ba4c5b857269195a828309e5cc5402055bd7f61
pchchv <ipchchv@gmail.com>
2023-04-01 09:53:59 +0300

    Add visitor tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
@@ -29,5 +31,81 @@ func TestRateLimiter_Allow(t *testing.T) {
 	time.Sleep(interval)
 	if !l.allow() {
 		t.Error("Request after interval should be allowed")
+	}
+}
+
+func TestVisitor_ServeHTTP(t *testing.T) {
+	l := newRateLimiter(2)
+	v := newVisitor(l)
+
+	// Create test server
+	testServer := httptest.NewServer(v)
+	defer testServer.Close()
+
+	// Test successful request
+	resp, err := http.Get(testServer.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status %d; got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	// Test too many requests
+	resp, err = http.Get(testServer.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status %d; got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	resp, err = http.Get(testServer.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusTooManyRequests {
+		t.Errorf("Expected status %d; got %d", http.StatusTooManyRequests, resp.StatusCode)
+	}
+
+	// Test invalid method
+	req, err := http.NewRequest(http.MethodPost, testServer.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Expected status %d; got %d", http.StatusMethodNotAllowed, resp.StatusCode)
+	}
+
+	// Test invalid IP address
+	req, err = http.NewRequest(http.MethodGet, testServer.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.RemoteAddr = "notanipaddress"
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status %d; got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	// Test IPv6 address
+	req, err = http.NewRequest(http.MethodGet, testServer.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.RemoteAddr = "[::1]:1234"
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Expected status %d; got %d", http.StatusBadRequest, resp.StatusCode)
 	}
 }
